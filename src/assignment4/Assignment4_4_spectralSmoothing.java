ec44e7260a263a2c38c853728f6c78ec71ae0955
Gian Calgeer <gidoca@gmail.com>
2013-11-05 17:29:23 +0100

    Fix indentation

--- src/assignment4/Assignment4_4_spectralSmoothing.java
+++ src/assignment4/Assignment4_4_spectralSmoothing.java
@@ -33,8 +33,8 @@ public class Assignment4_4_spectralSmoothing {
 	
 	public static void main(String[] args)
 	{
-		//sphericalHarmonicsDemo();
-		spectralSmoothingDemo();
+		sphericalHarmonicsDemo();
+		//spectralSmoothingDemo();
 	}
 
 	private static void spectralSmoothingDemo(){
@@ -77,53 +77,53 @@ public class Assignment4_4_spectralSmoothing {
 	{
 		HalfEdgeStructure hs = new HalfEdgeStructure();
 		try {
-		WireframeMesh m = ObjReader.read("./objs/teapot.obj", true);
-		hs.init(m);
-	} catch (MeshNotOrientedException | DanglingTriangleException | IOException e) {
-		e.printStackTrace();
-		return;
-	}
-	
-	GLHalfEdgeStructure glMeshDiffuse = new GLHalfEdgeStructure(hs);
-	glMeshDiffuse.configurePreferredShader("shaders/trimesh_flat.vert", 
-			"shaders/trimesh_flat.frag", 
-			"shaders/trimesh_flat.geom");
-	
-	MyDisplay d = new MyDisplay();
-	d.addToDisplay(glMeshDiffuse);
-
-	CSRMatrix laplacian = LMatrices.symmetricCotanLaplacian(hs);
-	ArrayList<Float> eigenValues = new ArrayList<Float>();
-	ArrayList<ArrayList<Float>> eigenVectors = new ArrayList<ArrayList<Float>>();
-	try {
-		SCIPYEVD.doSVD(laplacian, "laplacianevd", 20, eigenValues, eigenVectors);
-	} catch (IOException e) {
-		e.printStackTrace();
-		return;
-	}
-	
-	for(int i = 0; i < eigenVectors.size(); i++)
-	{
-		final ArrayList<Float> eigenVector = eigenVectors.get(i);
-		final float minEV = Collections.min(eigenVector);
-		final float maxEV = Collections.max(eigenVector);
+			WireframeMesh m = ObjReader.read("./objs/sphere.obj", true);
+			hs.init(m);
+		} catch (MeshNotOrientedException | DanglingTriangleException | IOException e) {
+			e.printStackTrace();
+			return;
+		}
 		
-		GLHalfEdgeStructure glEV = new GLHalfEdgeStructure(hs);
-		glEV.configurePreferredShader("shaders/trimesh_flatColor3f.vert", "shaders/trimesh_flatColor3f.frag", "shaders/trimesh_flatColor3f.geom");
-		glEV.setName("EV " + i);
-		glEV.addElement(3, "color", new VertexAttribute() {
-			@Override
-			public float[] getAttribute(Vertex v) {
-				float[] out = new float[3];
-				float val = (eigenVector.get(v.index) - minEV) / (float) Math.max(maxEV - minEV, .0001f);
-				out[0] = (float) Math.min(2 * Math.max(val, 0.1), 0.8);
-				out[2] = (float) Math.min(2 * Math.max(1 - val, 0.1), 0.8);
-				out[1] = (float) Math.min(out[0], out[2]);
-				return out;
-			}
-		});
-		d.addToDisplay(glEV);
-	}
+		GLHalfEdgeStructure glMeshDiffuse = new GLHalfEdgeStructure(hs);
+		glMeshDiffuse.configurePreferredShader("shaders/trimesh_flat.vert", 
+				"shaders/trimesh_flat.frag", 
+				"shaders/trimesh_flat.geom");
+		
+		MyDisplay d = new MyDisplay();
+		d.addToDisplay(glMeshDiffuse);
+	
+		CSRMatrix laplacian = LMatrices.symmetricCotanLaplacian(hs);
+		ArrayList<Float> eigenValues = new ArrayList<Float>();
+		ArrayList<ArrayList<Float>> eigenVectors = new ArrayList<ArrayList<Float>>();
+		try {
+			SCIPYEVD.doSVD(laplacian, "laplacianevd", 20, eigenValues, eigenVectors);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		for(int i = 0; i < eigenVectors.size(); i++)
+		{
+			final ArrayList<Float> eigenVector = eigenVectors.get(i);
+			final float minEV = Collections.min(eigenVector);
+			final float maxEV = Collections.max(eigenVector);
+			
+			GLHalfEdgeStructure glEV = new GLHalfEdgeStructure(hs);
+			glEV.configurePreferredShader("shaders/trimesh_flatColor3f.vert", "shaders/trimesh_flatColor3f.frag", "shaders/trimesh_flatColor3f.geom");
+			glEV.setName("EV " + i);
+			glEV.addElement(3, "color", new VertexAttribute() {
+				@Override
+				public float[] getAttribute(Vertex v) {
+					float[] out = new float[3];
+					float val = (eigenVector.get(v.index) - minEV) / (float) Math.max(maxEV - minEV, .0001f);
+					out[0] = (float) Math.min(2 * Math.max(val, 0.1), 0.8);
+					out[2] = (float) Math.min(2 * Math.max(1 - val, 0.1), 0.8);
+					out[1] = (float) Math.min(out[0], out[2]);
+					return out;
+				}
+			});
+			d.addToDisplay(glEV);
+		}
 
 	}
 }
